5e471e37cebb8b5e7ffb3d984da34e20350e7d12
blackchestnut <alexander.kalinichev@gmail.com>
2025-04-08 21:53:08 +0300

    Updates

--- src/helpers/constants.js
+++
@@ -1,6 +0,0 @@
-//export const SUPPORT_EMAIL = "zakaz@tehtelecom.ru";
-export const SUPPORT_EMAIL = "toucan305@gmail.com";
-export const SUPPORT_EMAIL_MAILTO = `mailto:${SUPPORT_EMAIL}`;
-
-export const SUPPORT_PHONE = '+7 (977) 953 39 11';
-export const SUPPORT_PHONE_TEL = 'tel:+79779533911';
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // Импортируйте маршрутизатор
-
-import './assets/main.css';
-
-const app = createApp(App); // Создайте приложение
-app.use(router); // Используйте маршрутизатор
-app.mount('#app'); // Смонтируйте приложение
