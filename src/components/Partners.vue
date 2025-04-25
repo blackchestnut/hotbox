82f8beb0947def30d88d6520e279d7b0e497a3ca
toucan-v8 <toucan305@gmail.com>
2025-04-25 20:51:04 +0300

    Fix partners styles

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="partner-wrapper">
+  <article class="partner-wrapper page-container">
     <div class="partners">
       <div class="production">
         В производстве мы используем оборудование только проверенных
@@ -73,97 +73,41 @@
         class="baltur-logo"
       />
     </div>
-  </div>
+  </article>
 </template>
 
 <style scoped>
 .production {
   font-weight: 600; /* Semi Bold */
   font-size: 40px; /* Размер шрифта 40px */
-  width: 1123px;
   text-align: center;
   margin-bottom: 60px;
 }
 .text {
   font-size: 30px;
-  width: 1100px;
   text-align: center;
   margin-bottom: 150px;
 }
 .partners {
-  width: 1148px;
-  height: 1116.25;
   margin: 0 auto;
   text-align: center;
   justify-content: center;
 }
 .pictures {
-  display: flex; /* Включаем Flexbox */
-  flex-wrap: wrap; /* Позволяем элементам переноситься на новую строку */
-  justify-content: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  column-gap: 80px;
+  row-gap: 10px;
 }
 .pictures img {
-  margin: 80px; /* Отступы между изображениями */
-  box-sizing: border-box; /* Учитываем отступы в ширине */
-}
-.bosch-logo {
-  width: 300px;
-  height: 67.13px;
-}
-.legrand-logo {
-  width: 300px;
-  height: 74.25px;
-}
-.group-logo {
-  width: 300px;
-  height: 61.44px;
-}
-.abb-logo {
-  width: 300px;
-  height: 119.25px;
-}
-.caldera-logo {
-  width: 300px;
-  height: 102px;
-}
-.she-logo {
-  width: 300px;
-  height: 120px;
-}
-.siemens-logo {
-  width: 300px;
-  height: 168.75px;
-}
-.vector-logo {
-  width: 296.63px;
-  height: 88.46px;
-}
-.weishaupt-logo {
-  width: 300px;
-  height: 52.5px;
-}
-.temron-logo {
-  width: 300px;
-  height: 162.5px;
-}
-.cnp-logo {
-  width: 300px;
-  height: 123.75px;
-}
-.baltur-logo {
-  width: 300px;
-  height: 94px;
+  width: 100%;
 }
 
 @media (max-width: 430px) {
-  .partner-wrapper {
-    max-width: 100%;
-    margin: 0 auto;
-  }
   .partners {
     margin-top: 36px;
     padding: 0px 16px;
-    width: 90%;
     margin-left: 0px;
     margin-right: 0px;
   }
@@ -171,27 +115,19 @@
     font-size: 20px;
     font-weight: 600;
     text-align: left;
-    max-width: 300px;
     margin-bottom: 30px;
   }
   .text {
     font-size: 15px;
     margin-bottom: 36px;
     text-align: left;
-    max-width: 291px;
   }
   .pictures {
-    display: grid; /* Используем CSS Grid */
-    grid-template-columns: repeat(2, 1fr); /* Два столбика */
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 36px;
-    padding: 0px 16px;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 34px;
+    row-gap: 6px;
   }
-  .pictures img {
-    margin: 24px; /* Отступы между изображениями */
-    box-sizing: border-box; /* Учитываем отступы в ширине */
-  }
+
   /* Указываем порядок логотипов */
   .bosch-logo {
     order: 3;
@@ -228,55 +164,6 @@
   }
   .baltur-logo {
     order: 9;
-  }
-
-  .bosch-logo {
-    width: 126px;
-    height: 26.85px;
-  }
-  .legrand-logo {
-    width: 126px;
-    height: 32px;
-  }
-  .group-logo {
-    width: 120px;
-    height: 32px;
-  }
-  .abb-logo {
-    width: 126px;
-    height: 47.7px;
-  }
-  .caldera-logo {
-    width: 128px;
-    height: 40.8px;
-  }
-  .she-logo {
-    width: 128px;
-    height: 48px;
-  }
-  .siemens-logo {
-    width: 128px;
-    height: 67.5px;
-  }
-  .vector-logo {
-    width: 120px;
-    height: 45px;
-  }
-  .weishaupt-logo {
-    width: 126px;
-    height: 21px;
-  }
-  .temron-logo {
-    width: 128px;
-    height: 54px;
-  }
-  .cnp-logo {
-    width: 128px;
-    height: 49.5px;
-  }
-  .baltur-logo {
-    width: 126px;
-    height: 37.6px;
   }
 }
 </style>
