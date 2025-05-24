ec561a4cdde61677bcf23f8598bc8a727bcc243d
toucan-v8 <toucan305@gmail.com>
2025-05-24 21:16:52 +0300

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -2,10 +2,17 @@ import { SitemapStream, streamToPromise } from "sitemap";
 import { writeFileSync } from "fs";
 import { Readable } from "stream";
 import xmlFormatter from "xml-formatter";
+import { boilers } from "data";
 
 const pages = [
   { url: "/", changefreq: "daily", priority: 1.0 },
   { url: "/about", changefreq: "monthly", priority: 0.8 },
+
+  { url: "/news", changefreq: "monthly", priority: 0.8 },
+  { url: "/car", changefreq: "monthly", priority: 0.8 },
+  { url: "/service", changefreq: "monthly", priority: 0.8 },
+  { url: "/boiler-room", changefreq: "monthly", priority: 0.8 },
+  { url: "/privacy", changefreq: "monthly", priority: 0.8 },
 ];
 
 const hostname = "https://hotboxrf.ru";
